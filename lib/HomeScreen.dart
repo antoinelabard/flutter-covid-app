cbee40ae27cadb3d755bb1d562d3b2387e3fd7d6
antoine <antoine@labard.fr>
2021-07-14 11:31:35 +0200

    Moved Clippath widget into a new class.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid_19_app/widgets/Counter.dart';
+import 'package:flutter_covid_19_app/widgets/Header.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'Constants.dart';
 
@@ -13,54 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(children: [
-      ClipPath(
-        clipper: MyClipper(),
-        child: Container(
-          padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-          height: 350,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xff3383cd),
-                    Color(0xff11249f),
-                  ]),
-              image: DecorationImage(
-                  image: AssetImage("assets/images/virus.png"))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                  alignment: Alignment.topRight,
-                  child: SvgPicture.asset("assets/icons/menu.svg")),
-              SizedBox(
-                height: 20,
-              ),
-              Expanded(
-                  child: Stack(
-                children: [
-                  SvgPicture.asset(
-                    "assets/images/Drcorona.png",
-                    width: 230,
-                    fit: BoxFit.fitWidth,
-                    alignment: Alignment.topCenter,
-                  ),
-                  Positioned(
-                      top: 20,
-                      left: 150,
-                      child: Text(
-                        'All you need is\nto stay at home.',
-                        style: kHeadingTextStyle.copyWith(color: Colors.white),
-                      )),
-                  Container()
-                ],
-              ))
-            ],
-          ),
-        ),
-      ),
+      Header(),
       Container(
         height: 60,
         width: double.infinity,
@@ -190,20 +144,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     ]));
   }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
